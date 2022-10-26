14d4ef8374c43062f77560b90cca82bada7ee1db
charith1997 <charithmadu97@gmail.com>
2022-10-26 21:44:16 +0530

    Routing implemented

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import logo from "./logo.svg";
-import "./App.css";
+import Header from "./components/Header";
+import {
+    BrowserRouter,
+    Routes,
+    Route
+} from "react-router-dom";
+import Organizations from "./components/Organizations";
+import Users from "./components/Users";
+import Overview from "./components/Overview";
 
 function App() {
-  return <div></div>;
+    return <div>
+        <BrowserRouter>
+            <Header/>
+            <Routes>
+                <Route path="/" element={<Overview/>}/>
+                <Route path="/organizations" element={<Organizations/>}/>
+                <Route path="/users" element={<Users/>}/>
+            </Routes>
+        </BrowserRouter>
+    </div>;
 }
 
 export default App;
